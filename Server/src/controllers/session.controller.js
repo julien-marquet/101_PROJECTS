c517ba984ca24c0bdb9099b6609061f3f6c10f8b
PCJULIEN\Julien_Marquet <julienmarquet9@gmail.com>
2018-02-17 15:10:58 +0100

    top level error handler

--- Server/src/controllers/session.controller.js
+++ Server/src/controllers/session.controller.js
@@ -1,4 +1,5 @@
 const utilities = require('../utilities/session');
+const helpers = require('../utilities/helpers');
 const errors = require('restify-errors');
 
 module.exports = sessions => ({
@@ -7,23 +8,20 @@ module.exports = sessions => ({
         if (req.query.code) {
             try {
                 const token = await utilities.get42UserToken(req.query.code);
-                sessions.registerSession(token).then((userSession) => {
+                try {
+                    const userSession = await sessions.registerSession(token);
                     res.toSend = {
                         ...res.toSend,
                         session: userSession,
                     };
                     next();
-                }).catch((err) => {
-                    req.log.error(err);
-                    next(err);
-                });
+                } catch (err) {
+                    next(helpers.handleErrors(req, err));
+                }
             } catch (err) {
-                const error = errors.makeErrFromCode(err.statusCode, JSON.stringify(err.error));
-                req.log.debug(error);
-                next(error);
+                next(helpers.handleErrors(req, err));
             }
         } else {
-            req.log.debug('Missing parameter');
             next(new errors.BadRequestError('Missing parameter'));
         }
     },
--- Server/src/utilities/helpers.js
+++ Server/src/utilities/helpers.js
@@ -1,3 +1,5 @@
+const errors = require('restify-errors');
+
 module.exports = {
     cleanLeanedResult(input) {
         if (Array.isArray(input)) {
@@ -20,4 +22,20 @@ module.exports = {
         delete newObj.__v; // eslint-disable-line no-underscore-dangle
         return (newObj);
     },
+    handleErrors(req, err) {
+        let res = {};
+        if (!err) {
+            res = new errors.InternalError('Unknown error');
+        } else if (!err.statusCode) {
+            res = new errors.InternalError(JSON.stringify(err.error));
+        } else {
+            res = errors.makeErrFromCode(err.statusCode, JSON.stringify(err.error));
+        }
+        if (res instanceof errors.InternalError) {
+            req.log.error(res);
+        } else {
+            req.log.debug(res);
+        }
+        return (res);
+    }
 };
