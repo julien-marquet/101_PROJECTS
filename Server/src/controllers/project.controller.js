eee42b6d15c6d337ccb0caeb1f7a23795874739c
PCJULIEN\Julien_Marquet <julienmarquet9@gmail.com>
2018-02-20 21:27:45 +0100

    Rename schema

--- Server/src/controllers/project.controller.js
+++ Server/src/controllers/project.controller.js
@@ -2,7 +2,7 @@ const errors = require('restify-errors');
 
 module.exports = (sessions, validator) => ({
     async post(req, res, next) {
-        if (validator.validate('projectPost', req.body)) {
+        if (validator.validate('project.post', req.body)) {
             res.toSend = {
                 message: 'ok',
             };
