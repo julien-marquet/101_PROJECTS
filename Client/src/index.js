a59066f46f592909615055445c524a67ae0b12cf
Leo Grivel <legrivel@student.le-101.fr>
2018-03-28 00:42:15 +0200

    Client created

---
+++ Client/src/index.js
@@ -0,0 +1,24 @@
+import {render} from "inferno";
+import {Provider} from "inferno-redux";
+import createSagaMiddleware from "redux-saga";
+import {
+    createStore,
+    applyMiddleware,
+    compose
+} from "redux";
+
+import reducers from "./app/reducers/index";
+import sagas from "./app/sagas/index";
+import App from "./App";
+import "./index.css";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, compose(applyMiddleware(sagaMiddleware)));
+sagaMiddleware.run(sagas);
+
+render(
+    (<Provider store={store}>
+        <App />
+    </Provider>),
+    document.getElementById("app")
+);
