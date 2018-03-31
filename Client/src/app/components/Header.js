7cdc9ba2b22a926806cb2cb85b1be62e25a725e4
Léo <leo.grivel@epitech.eu>
2018-03-31 21:32:04 +0200

    Added login functionnality

--- Client/src/app/components/Header.js
+++ Client/src/app/components/Header.js
@@ -1,11 +1,38 @@
 import {Component} from "inferno";
 
 import "../../css/general.css";
+import "../../css/header.css";
+import config from "../../config/config";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.login = this.login.bind(this);
+    }
+
+    login() {
+        window.location.replace(`${config.apiEndpoint}/oauth/authorize?client_id=${config.clientId}&redirect_uri=${config.redirectUri}&response_type=code`);
+    }
+
     render() {
         return (
-            <div className="container-fluid header">
+            <div className={"container-fluid header"}>
+                <div className={"row headerWrapper align-items-center"}>
+                    <div className={"col-4"}></div>
+                    <div className={"col-2"}></div>
+                    <div className={"col-2"}></div>
+                    <div className={"col-2"}></div>
+                    <div className={"col-2 "}>
+                        <button
+                            type={"button"}
+                            className={"btn btn-dark w-50"}
+                            onClick={this.login}
+                        >
+                            Login
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ Client/src/config/config.js
@@ -0,0 +1,5 @@
+export default {
+    apiEndpoint: "https://api.intra.42.fr",
+    clientId: "6a70908edc7b210ad935bfeb58ec10870f4f5c9ed4072356aa4a97ea3ff42bf8",
+    redirectUri: "http://localhost:3000/",
+};
